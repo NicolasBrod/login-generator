418a73b20f6771165daa9d62b0e43ca0b5ee4a53
NicolasBroders <@NicolasBroders>
2018-11-22 14:41:56 +0100

    #2 test generateLoginForNomAndPrenom

---
+++ src/test/java/geco/LoginGeneratorTest.java
@@ -0,0 +1,28 @@
+package geco;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LoginGeneratorTest {
+
+    LoginService loginService;
+    LoginGenerator loginGenerator;
+
+    @Before
+    public void init(){
+        loginService = new LoginService(new String[]
+                {"JROL","BPER","CGUR","JDU","JRAL","JRAL1"});
+    }
+
+    @Test
+    public void generateLoginForNomAndPrenomCas1() {
+        //Given new login generate
+        //When
+        //Then the result should be "PDUR"
+        loginGenerator = new LoginGenerator(loginService);
+        loginGenerator.generateLoginForNomAndPrenom("Durand","Paul");
+        assertEquals(true,loginService.loginExists("PDUR"));
+    }
+}
